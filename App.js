099976503e2da6866912f12cadbeffc7eb70370e
IliasPHP <ilias.elmabrak16@gmail.com>
2021-12-03 13:13:47 +0100

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 // import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, StatusBar  } from 'react-native';
+import { Searchbar,Button } from 'react-native-paper';
 
 export default function App() {
   return (
     <SafeAreaView style={{flex:1}}>
         <StatusBar />
-        <View  style={{ padding:16, backgroundColor:'green',}}>  
-          <Text>Search</Text>
+        <View  style={{ backgroundColor:'white'}}>
+          <Searchbar
+          placeholder={'search'}
+          />
         </View>
         <View  style={{flex:1, padding:16, backgroundColor:'lightgrey',}}>  
           <Text>List</Text>
